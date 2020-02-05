f2657cd6352e8aa629c78e3f21ac4d599be9b400
elbk201 <55895933+elbk201@users.noreply.github.com>
2020-02-04 17:27:01 -0800

    Create programa6.cpp

---
+++ programa6.cpp
@@ -0,0 +1,15 @@
+# convertirpulgadasayardasypiesapulgadas.cpp
+using namespace std;
+int main(int argc, char**argv)
+{
+int pulgadas, yardas,pies,resultpies;
+cout<<"Ingrese el numero de pulgadas: "<<ENDL;
+cout<<"Ingrese el numero de pies: "<<ENDL;
+cin>>pulgadas;
+cin>>pies
+yardas = pulgadas * 1.0 / 36;
+resultpies = pies * 12;
+cout<<"\nLas yardas son: "<<yardas;
+cout<<"\nLas pulgadas son: "<<resultpies;
+return 0;
+}
